b864b44c3c971f009e334244a9598c7828703e09
Dj0nny <frank96990@gmail.com>
2019-04-11 16:18:05 +0200

    :pencil2: edit App.vue and :sparkles: add QRCodeGenerator.vue

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,37 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>QR Code</span>
+        <span class="font-weight-light">Generator</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/dj0nny/qrcode-generator"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">View repository on Github</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <QRCodeGenerator />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import QRCodeGenerator from './components/QRCodeGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QRCodeGenerator
   },
   data () {
     return {
-      //
+      
     }
   }
 }
---
+++ src/components/QRCodeGenerator.vue
@@ -0,0 +1,14 @@
+<template>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'QRCodeGenerator'
+}
+</script>
+
+<style>
+
+</style>
