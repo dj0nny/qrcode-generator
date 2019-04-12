9ea5b171a6887eabbc7677078610a488a47a9821
Dj0nny <frank96990@gmail.com>
2019-04-12 14:02:39 +0200

    :pencil2: edit QRCodeGenerator.vue and :sparkles: QRCode.vue

---
+++ src/components/QRCode.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-flex xs12>
+    {{ payload }}    
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'QRCode',
+  props: ['payload']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/QRCodeGenerator.vue
+++ src/components/QRCodeGenerator.vue
@@ -1,11 +1,46 @@
 <template>
+  <v-container text-xs-center>
+    <v-layout>
+      <v-flex xs12>
+        <v-form v-model="valid" @submit.prevent="generateQR" @keydown.prevent.enter>
+          <v-text-field v-model="url" :rules="notEmptyRules" label="URL" required></v-text-field>
+          <v-btn type="submit" :disabled="!valid">Generate QR Code</v-btn>
+        </v-form>
+        <v-btn type="submit" @click="resetForm" >Reset Form</v-btn>
+      </v-flex>
+    </v-layout>
 
-  
+    <v-layout v-if="submitted">
+      <QRCode :payload="url" />
+    </v-layout> 
+  </v-container>
 </template>
 
 <script>
+import QRCode from './QRCode.vue'
+
 export default {
-  name: 'QRCodeGenerator'
+  name: 'QRCodeGenerator',
+  components: {
+    QRCode
+  },
+  data: (vm) => ({
+    valid: false,
+    submitted: false,
+    url: '',
+    notEmptyRules: [(value) => !!value || "Cannot be empty"]
+  }),
+  methods: {
+    generateQR() {
+      if(this.valid) {
+        this.submitted = true
+      }
+    },
+    resetForm() {
+      this.valid = false;
+      this.url = '';
+    }
+  }
 }
 </script>
 
